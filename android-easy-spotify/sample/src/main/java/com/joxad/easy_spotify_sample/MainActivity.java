84f428dff112658f896b7c2bb5a9da4d57080f10
Josh <jdavid@phoceis.com>
2016-03-10 14:53:45 +0100

    Sample

--- android-easy-spotify/sample/src/main/java/com/joxad/easy_spotify_sample/MainActivity.java
+++ android-easy-spotify/sample/src/main/java/com/joxad/easy_spotify_sample/MainActivity.java
@@ -1,13 +1,67 @@
 package com.joxad.easy_spotify_sample;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.joxad.android_easy_spotify.SpotifyManager;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String TAG = MainActivity.class.getSimpleName();
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = (TextView) findViewById(R.id.tv);
+        try {
+            new SpotifyManager.Builder()
+                    .setContext(this)
+                    .setApiCallback(getString(R.string.api_spotify_callback))
+                    .setApiKey(getString(R.string.api_spotify_id))
+                    .setScope(new String[]{"user-read-private", "streaming"})
+                    .setConnectionType(AuthenticationResponse.Type.CODE)
+                    .build();
+        } catch (Exception e) {
+            Log.d(TAG, e.getLocalizedMessage());
+        }
+
+        SpotifyManager.loginWithActivity(new SpotifyManager.OAuthListener() {
+            @Override
+            public void onReceived(String code) {
+                Log.d(TAG, code);
+               tv.setText(code);
+            }
+
+            @Override
+            public void onError(String error) {
+                Log.d(TAG, error);
+                tv.setText(error);
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        SpotifyManager.onActivityResult(requestCode,resultCode,data);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        SpotifyManager.onNewIntent(intent);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        SpotifyManager.destroy();
     }
 }
